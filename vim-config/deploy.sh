c5e1569d7592d6adb50808c51aea9e043c6b5717
CMelina <allen.586@wright.edu>
2026-02-20 14:10:35 -0500

    Task 1 of Dotfiles

---
+++ vim-config/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp dotfiles.vimrc ~/.vimrc
+
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
