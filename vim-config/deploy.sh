d001e36468bdc068103f2da48cb1081f09b9af9b
CMelina <allen.586@wright.edu>
2026-04-23 12:03:39 -0400

    Update deploy.sh

--- vim-config/deploy.sh
+++ vim-config/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Copies local vimrc configuration to the home directory
 cp dotfiles.vimrc ~/.vimrc
 
+# Check if Vundle is installed (if not, clone it from github repo)
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+# run to install all plugins defined in .vimrc
 vim +PluginInstall +qall
